0eae181089788c6484ba666877604569989df67a
Nicolas <nico.hhara@gmail.com>
2026-04-22 12:08:14 -0300

    n

---
+++ src/Titulo.jsx
@@ -0,0 +1,10 @@
+function Titulo(){
+    return (
+        <div>
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem cupiditate ex quibusdam nam eaque. Et nisi quam corporis earum odit quisquam! Tempore laborum omnis asperiores non dolor perferendis porro!</p>
+        </div>
+    )
+}
+
+export default Titulo
