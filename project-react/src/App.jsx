14bf08d9564146c6c4dbf044b44d08574371ea32
João Vitor Silva Machado <86895133020@senacrs.edu.br>
2026-04-28 11:42:55 -0300

    1

---
+++ project-react/src/App.jsx
@@ -0,0 +1,17 @@
+function MyButton() {
+  return (
+    <button>
+      buy something
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my store</h1>
+      <h2>liberal shop make you free</h2>
+      <MyButton />
+    </div>
+  );
+}
